5d4c31a3a349665918ad846e19600374d69d0f82
mario <mariotomicx@gmail.com>
2022-07-15 14:58:30 +0200

    add title length validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { in: 10...30 }
   validates :image_url, allow_blank: true, format: {
     with:   %r{\w+.(jpg|png|gif)\z}i,
     message: 'Must be a URL for GIF, JPG or PNG image.'
